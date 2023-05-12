2a60cc88f8fe8c9159638f4c7c258cf1e41c486b
TigranBalayan10 <tigranbalayan10@gmail.com>
2023-05-11 20:11:46 -0700

    form submit functionality added

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { useParams, Navigate } from "react-router-dom";
 
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from '../components/ThoughtForm';
 import { ADD_FRIEND } from "../utils/mutations";
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
@@ -72,6 +73,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
